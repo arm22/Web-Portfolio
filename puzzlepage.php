4bf2259db4aeb72790e4ef875a325f95f5d36d76
Austin Meyers <meyersaustin22@gmail.com>
2016-02-02 09:21:11 -0800

    Added header nav

--- puzzlepage.php
+++ puzzlepage.php
@@ -21,6 +21,31 @@
 		<link rel="Shortcut Icon" type="image/ico" href="transparent.ico" />
 	</head>
 	<body>
+	<div class="bs-example">
+    <nav role="navigation" class="navbar navbar-default">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Brand</a>
+        </div>
+        <!-- Collection of nav links and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Messages</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
 		<div class="container-fluid">
 			<div class="row" id="three">
 				<div class="col-md-4 left">
