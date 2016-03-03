10ae47519f157ec4367452c56f0369e8938b9dd7
Austin Meyers <meyersaustin22@gmail.com>
2016-03-03 09:55:53 -0800

    Remove empty folder

---
+++ functions/functions.php
@@ -0,0 +1,12 @@
+<?php
+  function fill_head() { ?>
+    <meta name="google-site-verification" content="tY_4djMl2E9ysu0JXCfs0gmnZeOrjj3P4ly4fpfZ_bc" />
+    <meta name="Austin Meyers | Developer" content="Personal web-page of Austin R. Meyers." />
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/bower_components/material-design-lite/material.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link href="css/index.css" type="text/css" rel="stylesheet" />-->
+    <title>Austin Meyers | Developer</title>
+    <link rel="Shortcut Icon" type="image/ico" href="img/transparent.ico" />
+  <?php  } ?>
