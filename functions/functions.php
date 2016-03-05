fed3ef03f82680da671ce20deb7d1907fed04bef
Austin Meyers <meyersaustin22@gmail.com>
2016-03-04 20:21:35 -0800

    Module footer

--- functions/functions.php
+++ functions/functions.php
@@ -9,4 +9,25 @@
     <!--<link href="css/index.css" type="text/css" rel="stylesheet" />-->
     <title>Austin Meyers | Developer</title>
     <link rel="Shortcut Icon" type="image/ico" href="img/transparent.ico" />
-  <?php  } ?>
+  <?php  } 
+    function fill_foot() { ?>
+        <div class="row" id="footer">
+            <div class="col-md-6">
+                <div>
+                    <img src="img/rasbpi.png" alt="logo" class="img-responsive center" width="42px" height="53px" />
+                    <p>100% Locally Hosted.</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div>
+                        <a href="http://stackoverflow.com/users/4403604/austin-meyers" target="_blank"><i class="fa fa-stack-overflow fa-2x"></i></a>
+                        <a href="https://www.linkedin.com/pub/austin-meyers/94/a1a/bb1" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                        <a href="https://github.com/arm22" target="_blank"><i class="fa fa-github-square fa-2x"></i></a>
+                        <p>Reach out & touch me.</p>
+                </div>
+            </div>
+        </div>
+        <script src="/bower_components/material-design-lite/material.min.js"></script>
+        <!-- Personal JavaScript -->
+        <script src="index.js" type="text/javascript"></script>
+<?php    } ?>
--- index.php
+++ index.php
@@ -104,25 +104,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="row" id="footer">
-				<div class="col-md-6">
-					<div>
-						<img src="img/rasbpi.png" alt="logo" class="img-responsive center" width="42px" height="53px" />
-						<p>100% Locally Hosted.</p>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div>
-							<a href="http://stackoverflow.com/users/4403604/austin-meyers" target="_blank"><i class="fa fa-stack-overflow fa-2x"></i></a>
-							<a href="https://www.linkedin.com/pub/austin-meyers/94/a1a/bb1" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
-							<a href="https://github.com/arm22" target="_blank"><i class="fa fa-github-square fa-2x"></i></a>
-							<p>Reach out & touch me.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<script src="/bower_components/material-design-lite/material.min.js"></script>
-		<!-- Personal JavaScript -->
-		<script src="index.js" type="text/javascript"></script>
+      <?php fill_foot(); ?>
 	</body>
 </html>
