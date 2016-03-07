d8d7224650f15ee74bdec7be326371e99b29138b
Austin Meyers <meyersaustin22@gmail.com>
2016-03-06 18:05:54 -0800

    Fix puzzle page

--- puzzlepage.php
+++ puzzlepage.php
@@ -55,25 +55,11 @@
 				<p><a href="http://www.austinrmeyers.com/puzzlepage.php"><i class="fa fa-refresh fa-2x"></i></a></p>
 			</div>
 			</div>
-			<div class="row" id="footer">
-				<div class="col-md-6">
-					<div>
-						<img src="img/rasbpi.png" alt="logo" class="img-responsive center" width="42px" height="53px" />
-						<p>100% Locally Hosted.</p>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div>
-							<a href="http://stackoverflow.com/users/4403604/austin-meyers" target="_blank"><i class="fa fa-stack-overflow fa-2x"></i></a>
-							<a href="https://www.linkedin.com/pub/austin-meyers/94/a1a/bb1" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
-							<a href="https://github.com/arm22" target="_blank"><i class="fa fa-github-square fa-2x"></i></a>
-							<p>Reach out & touch me.</p>
-					</div>
-				</div>
+			<?php
+			fill_foot();
+			?>
 			</div>
-		</div>
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+			
 		<!-- Personal JavaScript -->
 		<script src="js/puzzle.js" type="text/javascript"></script>
 	</body>
